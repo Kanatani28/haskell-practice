35e81ecea3a0b42eb150d6439ed5a0b8b00ca670
maroKanatani <16130443+Kanatani28@users.noreply.github.com>
2021-12-12 14:22:21 +0900

    02 修正

--- 02_longest_common_prefix/index.hs
+++ 02_longest_common_prefix/index.hs
@@ -3,12 +3,14 @@ main = do
   putStr "Ex1: [\"flower\",\"flow\",\"flight\"] is "
   print (longestCommonPrefix ["flower", "flow", "flight"])
   putStr "Ex2: [\"dog\",\"racecar\",\"car\"] is "
-  print (longestCommonPrefix ["dog", "racer", "car"])
+  print (longestCommonPrefix ["dog", "racecar", "car"])
 
 longestCommonPrefix :: [String] -> String
 longestCommonPrefix [] = ""
 longestCommonPrefix [x] = x
-longestCommonPrefix (x : xs) = foldr1 prefix xs
+longestCommonPrefix x = foldr1 prefix x
+-- 通常のfoldrを使う場合は明示的に先頭要素を指定する必要あり
+-- longestCommonPrefix (x : xs) = foldr prefix x xs
 
 prefix :: String -> String -> String
 prefix x [] = ""
