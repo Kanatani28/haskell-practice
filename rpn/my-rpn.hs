1ac65e33ee88e0d3178eab476839e0e5f9e97f32
maroKanatani <16130443+Kanatani28@users.noreply.github.com>
2022-05-23 11:44:10 +0900

    Update my-rpn.hs

--- rpn/my-rpn.hs
+++ rpn/my-rpn.hs
@@ -1,1 +1,39 @@
+main::IO()
+
+main = do
+    -- let input = "10 4 3 + 2 * -"
+    let input = "10 4 3 - 2 * -"
+    let items = words input
+    print $ calculate [] items
+    
+
+calculate :: [String] -> [String] -> Int
+
+calculate stack (y:ys)
+  | isOperator y = calculate newStack ys 
+  | otherwise = calculate (stack ++ y : []) ys
+  where 
+    newStack = (init $ init stack) ++ ((show $ calc y $ toComputable $ takeLast 2 stack) : [])
+calculate [x] [] = read x
+
+takeLast :: Int -> [a] -> [a]
+takeLast n x = reverse $ take n $ reverse x 
+
+toComputable :: [String] -> (Int, Int)
+toComputable [x, y] = (read x,  read y)
+-- toComputable _ = (0, 0)
+
+isOperator :: String -> Bool
+isOperator "+" = True
+isOperator "-" = True
+isOperator "*" = True
+isOperator "/" = True
+isOperator _ = False
+
+calc :: String -> (Int, Int) -> Int
+calc "+" (x, y) = x + y
+calc "-" (x, y) = x - y
+calc "*" (x, y) = x * y
+-- calc "/" (x, y) = x / y
+calc _ _= 0
 
